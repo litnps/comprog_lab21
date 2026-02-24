dc7f173f95438d534d63863100293def25d19c92
litnps <lalitth21.n@gmail.com>
2026-02-24 15:19:47 +0700

    Q2 done

--- average.cpp
+++ average.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<cmath>
 
+using namespace std;
+
+int main(int argc, char * argv[]){
+    if(argc == 1){
+        cout<<"Please input numbers to find average.";
+        return 0;
+    }
+    cout<<"---------------------------------\n";
+    
+    double sum = 0;
+    for(int i = 1 ; i < argc ; ++i){
+        sum += atof(argv[i]);
+    }
+    argc--;
+    
+    cout<<"Average of "<<argc<<" numbers = "<<sum/argc<<"\n";
+
+    cout<<"---------------------------------\n";
+    
+
+    return 0;
+}
